1b978ed577b19616e353a64232232c804da50a4c
Agata Szwerbel <agata.szwerbel@gmail.com>
2022-08-02 22:05:46 +0200

    toggle task done

--- js/script.js
+++ js/script.js
@@ -6,24 +6,53 @@
     },
   ];
 
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+    render();
+  };
+
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  };
+
+  const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
+  };
+
   const render = () => {
     let htmlString = "";
 
     for (const task of tasks) {
       htmlString += `
             <li ${task.done ? ' style="text-decoration: line-through"' : ""}>
+            <button class="js-done">zrobione?</button>
+            <button class="js-remove">usuń</button>
             ${task.content}
             </li>
             `;
     }
     document.querySelector(".js-tasks").innerHTML = htmlString;
-  };
 
-  const addNewTask = (newTaskContent) => {
-    tasks.push({
-      content: newTaskContent,
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(index);
+      });
     });
-    render();
+
+    const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+    toggleDoneButtons.forEach((toggleDoneButton, index) => {
+      toggleDoneButton.addEventListener("click", () => {
+        toggleTaskDone(index);
+        
+      });
+    });
   };
 
   const onFormSubmit = (event) => {
